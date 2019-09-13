59ccd69db0d61f8f6840999e412a948fea81c748
Rosano Coutinho <pub@xeari.com>
2019-09-13 18:14:45 -0400

    Spec OLSKRemoteStorageJSONSchema

---
+++ main-tests.js
@@ -0,0 +1,124 @@
+const { throws, deepEqual } = require('assert');
+
+const mainModule = require('./main.js');
+
+describe('OLSKRemoteStorageJSONSchema', function OLSKRemoteStorageJSONSchema() {
+
+	it('throws error if not object', function() {
+		throws(function() {
+			mainModule.OLSKRemoteStorageJSONSchema(null);
+		}, /OLSKErrorInputInvalid/);
+	});
+
+	it('returns object', function() {
+		deepEqual(mainModule.OLSKRemoteStorageJSONSchema({}), {
+			type: 'object',
+			properties: {},
+			required: [],
+		});
+	});
+
+	context('properties', function() {
+		
+		it('declares string', function() {
+			deepEqual(mainModule.OLSKRemoteStorageJSONSchema({
+				alfa: ['XYZErrorNotString']
+			}), {
+				type: 'object',
+				properties: {
+					alfa: {
+						type: 'string',
+					},
+				},
+				required: [
+					'alfa',
+				],
+			});
+		});
+		
+		it('declares boolean', function() {
+			deepEqual(mainModule.OLSKRemoteStorageJSONSchema({
+				alfa: ['XYZErrorNotBoolean']
+			}), {
+				type: 'object',
+				properties: {
+					alfa: {
+						type: 'boolean',
+					},
+				},
+				required: [
+					'alfa',
+				],
+			});
+		});
+		
+		it('declares date', function() {
+			deepEqual(mainModule.OLSKRemoteStorageJSONSchema({
+				alfa: ['XYZErrorNotDate']
+			}), {
+				type: 'object',
+				properties: {
+					alfa: {
+						type: 'string',
+						format: 'date-time',
+					},
+				},
+				required: [
+					'alfa',
+				],
+			});
+		});
+		
+		it('declares filled', function() {
+			deepEqual(mainModule.OLSKRemoteStorageJSONSchema({
+				alfa: ['XYZErrorNotFilled']
+			}), {
+				type: 'object',
+				properties: {
+					alfa: {
+						type: 'string',
+					},
+				},
+				required: [
+					'alfa',
+				],
+			});
+		});
+		
+	});
+
+	context('required', function() {
+		
+		it('declares if required', function() {
+			deepEqual(mainModule.OLSKRemoteStorageJSONSchema({
+				alfa: ['XYZErrorNotString']
+			}), {
+				type: 'object',
+				properties: {
+					alfa: {
+						type: 'string',
+					},
+				},
+				required: [
+					'alfa',
+				],
+			});
+		});
+
+		it('ignores', function() {
+			deepEqual(mainModule.OLSKRemoteStorageJSONSchema({
+				alfa: ['XYZErrorNotString', '__RSOptional']
+			}), {
+				type: 'object',
+				properties: {
+					alfa: {
+						type: 'string',
+					},
+				},
+				required: [],
+			});
+		});
+		
+	});
+
+});
